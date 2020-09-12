c0eee7ae9643455af0061313ee2972587bbd4361
Rian Prakoso <mohammad.rian.ap@gmail.com>
2020-09-13 01:48:22 +0700

    initial commit

---
+++ app/src/main/java/com/mrap/vicam/MainActivity.java
@@ -0,0 +1,41 @@
+package com.mrap.vicam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.otaliastudios.cameraview.CameraView;
+
+public class MainActivity extends Activity {
+
+    CameraView camera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        camera = findViewById(R.id.camera);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        camera.open();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        camera.close();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        camera.destroy();
+    }
+}
